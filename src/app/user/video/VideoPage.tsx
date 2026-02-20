695b51de87e02366424626d2439c9d4beb2de848
vyshnavirakvih-cyber <vyshnavi.rakvih@gmail.com>
2026-02-20 20:36:04 +0530

    Initial commit

--- src/app/user/video/VideoPage.tsx
+++ src/app/user/video/VideoPage.tsx
@@ -181,7 +181,7 @@ export default function VideoPage() {
         ...prev,
         [activeVid.uniqueId]: (prev[activeVid.uniqueId] || 0) + 1,
       }));
-    }, 1000); // real 2 sec
+    }, 1000); // real 1 sec
 
     return () => clearTimeout(timer);
   }, [activeIndex, user]);
