8faa53fb5f08d2ffd40dc4f37715a81929f2e985
bookdot444 <bookdot.444@gmail.com>
2026-02-25 13:39:57 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,18 +3,11 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.qicktick.app',
   appName: 'Qicktick',
-  webDir: 'out', // leave this, but it won’t be used
+  webDir: 'out',
   server: {
-    url: 'https://qicktick.com/',
-    cleartext: true
-  },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 2000,
-      backgroundColor: "#ffffff",
-      showSpinner: true,
-      androidScaleType: "CENTER_CROP"
-    }
+    url: 'https://qicktick.com',
+    cleartext: false, // VERY IMPORTANT
+    allowNavigation: ['qicktick.com', '*.qicktick.com']
   }
 };
 
